8451fc02da05864cfa65a2c4caaed1450fe0f5b3
Peter Winskill <p.winskill@imperial.ac.uk>
2020-08-19 16:44:58 +0100

    Allow for an empty index when updating variable

--- R/api.R
+++ R/api.R
@@ -16,7 +16,7 @@ SimAPI <- R6::R6Class(
       state_names <- vcapply(unlist(list(...)), function(s) s$name)
       process_get_state(private$.api, individual$name, state_names)
     },
-
+    
     #' @description Get a variable vector for an individual
     #' @param individual the individual of interest
     #' @param variable the variable of interest
@@ -34,7 +34,7 @@ SimAPI <- R6::R6Class(
         index
       )
     },
-
+    
     #' @description Queue a state update for the end of the timestep
     #' @param individual the individual of interest
     #' @param state the target state
@@ -47,7 +47,7 @@ SimAPI <- R6::R6Class(
         index
       )
     },
-
+    
     #' @description Queue an update for a variable. There are 4 types of variable update:
     #'
     #' 1. Subset update. The index vector represents a subset of the variable to
@@ -65,66 +65,85 @@ SimAPI <- R6::R6Class(
     #' @param values a vector or scalar of values to assign at the index
     #' @param index is the index at which to apply the change, use NULL for the
     #' fill options
-    queue_variable_update = function(individual, variable, values, index=numeric(0)) {
-      process_queue_variable_update(
-        private$.api,
-        individual$name,
-        variable$name,
-        index,
-        values
-      )
-    },
-
-    #' @description Schedule an event to occur in the future
-    #' @param event the event to schedule
-    #' @param target the individuals to pass to the listener
-    #' @param delay the number of timesteps to wait before triggering the event
-    schedule = function(event, target, delay) {
-      process_schedule(private$.api, event$name, target, delay)
-    },
-
-    #' @description Get the individuals who are scheduled for a particular event
-    #' @param event, the event of interest
-    get_scheduled = function(event) {
-      process_get_scheduled(private$.api, event$name)
-    },
-
-    #' @description Stop a future event from triggering for a subset of individuals
-    #' @param event the event to stop
-    #' @param target the individuals to clear
-    clear_schedule = function(event, target) {
-      process_clear_schedule(private$.api, event$name, target)
-    },
-
-    #' @description Get the current timestep of the simulation
-    get_timestep = function() {
-      process_get_timestep(private$.api)
-    },
-
-    #' @description Get the parameters of the simulation
-    get_parameters = function() {
-      private$.parameters
-    },
-
-    #' @description Add a value for the simulation output
-    #' @param name the column name
-    #' @param value the value to assign
-    #' @param timestep optionally add the value to a specific timestep
-    render = function(name, value, timestep=NULL) {
-      if (is.null(timestep)) {
-        timestep <- self$get_timestep()
+    queue_variable_update = function(individual, variable, values, index=NULL) {
+      if(is.null(index)){
+        if(length(values) == 1){
+          process_queue_variable_fill(
+            private$.api,
+            individual$name,
+            variable$name,
+            values
+          )
+        } else {
+          process_queue_variable_update(
+            private$.api,
+            individual$name,
+            variable$name,
+            numeric(0),
+            values
+          )
+        }
+      } else if(length(index) != 0) {
+          process_queue_variable_update(
+            private$.api,
+            individual$name,
+            variable$name,
+            index,
+            values
+        )
       }
-      private$.renderer$add(name, value, timestep)
     },
-
-    #' @description Create an R wrapper for the API
-    #' @param cpp_api the cpp implementation of the simulation api
-    #' @param parameters model parameters
-    #' @param renderer renderer to store model outputs to
-    initialize = function(cpp_api, parameters, renderer) {
-      private$.api <- cpp_api
-      private$.parameters <- parameters
-      private$.renderer <- renderer
-    }
+      
+      #' @description Schedule an event to occur in the future
+      #' @param event the event to schedule
+      #' @param target the individuals to pass to the listener
+      #' @param delay the number of timesteps to wait before triggering the event
+      schedule = function(event, target, delay) {
+        process_schedule(private$.api, event$name, target, delay)
+      },
+      
+      #' @description Get the individuals who are scheduled for a particular event
+      #' @param event, the event of interest
+      get_scheduled = function(event) {
+        process_get_scheduled(private$.api, event$name)
+      },
+      
+      #' @description Stop a future event from triggering for a subset of individuals
+      #' @param event the event to stop
+      #' @param target the individuals to clear
+      clear_schedule = function(event, target) {
+        process_clear_schedule(private$.api, event$name, target)
+      },
+      
+      #' @description Get the current timestep of the simulation
+      get_timestep = function() {
+        process_get_timestep(private$.api)
+      },
+      
+      #' @description Get the parameters of the simulation
+      get_parameters = function() {
+        private$.parameters
+      },
+      
+      #' @description Add a value for the simulation output
+      #' @param name the column name
+      #' @param value the value to assign
+      #' @param timestep optionally add the value to a specific timestep
+      render = function(name, value, timestep=NULL) {
+        if (is.null(timestep)) {
+          timestep <- self$get_timestep()
+        }
+        private$.renderer$add(name, value, timestep)
+      },
+      
+      #' @description Create an R wrapper for the API
+      #' @param cpp_api the cpp implementation of the simulation api
+      #' @param parameters model parameters
+      #' @param renderer renderer to store model outputs to
+      initialize = function(cpp_api, parameters, renderer) {
+        private$.api <- cpp_api
+        private$.parameters <- parameters
+        private$.renderer <- renderer
+      }
   )
 )
