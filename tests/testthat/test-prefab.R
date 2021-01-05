bd5a539a2408b6a8c10e672e1d50679fc34d7123
Giovanni Charles <giovanni.charles@gmail.com>
2021-01-05 17:48:13 +0000

    Sketch tests for refactor

--- tests/testthat/test-prefab.R
+++ tests/testthat/test-prefab.R
@@ -1,20 +1,10 @@
-test_that("fixed_probability_state_change moves a sane number of individuals around", {
-  S <- State$new('S', 10)
-  I <- State$new('I', 1)
-  human <- Individual$new('test', list(S, I))
-  sim <- setup_simulation(list(human))
-  execute_process(
-    fixed_probability_state_change_process(
-      'test',
-      'S',
-      'I',
-      .5
-    ),
-    sim$cpp_api
-  )
+test_that("bernoulli_process moves a sane number of individuals around", {
+  state <- CategoricalVariable(c('S', 'I'), c(rep('S', 10), 'I'))
+  execute_process(bernoulli_process(state, 'S', 'I', .5), 1)
+  state$.update()
   state_apply_updates(sim$state)
-  n_s <- length(sim$r_api$get_state(human, S))
-  n_i <- length(sim$r_api$get_state(human, I))
+  n_s <- length(state$get_index_for('S'))
+  n_i <- length(state$get_index_for('I'))
   expect_lte(n_s, 10)
   expect_gte(n_i, 1)
   expect_equal(n_s + n_i, 11)
@@ -71,60 +61,61 @@ test_that("fixed_probability_forked_state_change_process works properly", {
 
 
 test_that("update_state_listener updates the state correctly", {
-  S <- State$new('S', 10)
-  I <- State$new('I', 1)
-  event <- Event$new('event')
-  event$add_listener(update_state_listener('test', 'I'))
-  human <- Individual$new('test', list(S, I), events=list(event))
-  sim <- setup_simulation(list(human))
-  sim$r_api$schedule(event, c(2, 5), 1)
-  scheduler_tick(sim$scheduler)
-  scheduler_process_events(sim$scheduler, sim$cpp_api, sim$r_api)
-  state_apply_updates(sim$state)
-  expect_setequal(sim$r_api$get_state(human, S), c(1, 3:4, 6:10))
-  expect_setequal(sim$r_api$get_state(human, I), c(11, 2, 5))
+  state <- CategoricalVariable(c('S', 'I'), c(rep('S', 10), 'I'))
+  event <- Event$new()
+  event$add_listener(update_state_listener(state, 'I'))
+  event$schedule(c(2, 5), 1)
+  event$.tick()
+  event$.process()
+  state$.update()
+  expect_setequal(state$get_index_for('S'), c(1, 3:4, 6:10))
+  expect_setequal(state$get_index_for('I'), c(11, 2, 5))
 })
 
 test_that("reschedule_listener schedules the correct update", {
-  event <- Event$new('event')
-  followup <- Event$new('followup')
+  event <- Event$new()
+  followup <- Event$new()
   S <- State$new('S', 10)
-  human <- Individual$new('test', list(S), events=list(event, followup))
-  event$add_listener(reschedule_listener('followup', 1))
+  event$add_listener(reschedule_listener(followup, 1))
   event_listener <- mockery::mock()
   event$add_listener(event_listener)
   followup_listener <- mockery::mock()
   followup$add_listener(followup_listener)
-  sim <- setup_simulation(list(human))
-  scheduler <- sim$scheduler
+
   #time = 0
-  sim$r_api$schedule(event, c(2, 4), 2)
+  event$schedule(c(2, 4), 2)
 
   #time = 1
-  scheduler_process_events(scheduler, sim$cpp_api, sim$r_api)
+  event$.process()
+  followup$.process()
   mockery::expect_called(followup_listener, 0)
   mockery::expect_called(event_listener, 0)
-  scheduler_tick(scheduler)
+  event$.tick()
+  followup$.tick()
 
   #time = 2
-  scheduler_process_events(scheduler, sim$cpp_api, sim$r_api)
+  event$.process()
+  followup$.process()
   mockery::expect_called(followup_listener, 0)
   mockery::expect_called(event_listener, 0)
-  scheduler_tick(scheduler)
+  event$.tick()
+  followup$.tick()
 
   #time = 3
-  scheduler_process_events(scheduler, sim$cpp_api, sim$r_api)
-  mockery::expect_called(followup_listener, 0)
+  event$.process()
+  followup$.process()
   mockery::expect_called(event_listener, 1)
-  scheduler_tick(scheduler)
+  mockery::expect_called(followup_listener, 0)
+  event$.tick()
+  followup$.tick()
 
   #time = 4
-  scheduler_process_events(scheduler, sim$cpp_api, sim$r_api)
-  mockery::expect_called(followup_listener, 1)
+  event$.process()
+  followup$.process()
   mockery::expect_called(event_listener, 1)
-  mockery::expect_args(event_listener, 1, api = sim$r_api, target = c(2, 4))
-  args <- mockery::mock_args(followup_listener)
-  expect_equal(length(args), 1)
-  expect_equal(args[[1]][[1]], sim$r_api)
-  expect_setequal(args[[1]][[2]], c(2, 4))
+  mockery::expect_called(followup_listener, 1)
+  event$.tick()
+  followup$.tick()
+  mockery::expect_args(event_listener, 1, 4, target = c(2, 4))
+  mockery::expect_args(followup_listener, 1, 4, target = c(2, 4))
 })
--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -1,9 +1,4 @@
 test_that("Premature render works", {
-  S <- State$new('S', 10)
-  I <- State$new('I', 100)
-  R <- State$new('R', 0)
-  human <- Individual$new('human', list(S, I, R))
-
   render <- Render$new(2)
   render$add('human_S_count', 10, 1)
   render$add('human_I_count', 100, 1)
@@ -19,66 +14,54 @@ test_that("Premature render works", {
 })
 
 test_that("Prefab state counts work correctly", {
-  S <- State$new('S', 10)
-  I <- State$new('I', 100)
-  R <- State$new('R', 0)
-  human <- Individual$new('human', list(S, I, R))
+  state <- CategoricalVariable(c('S', 'I'), c(rep('S', 10), rep('I', 100)))
 
   render <- Render$new(2)
-  sim <- setup_simulation(list(human), renderer = render)
 
-  render_states <- state_count_renderer_process(
-    human$name,
-    c(S$name, I$name, R$name)
+  render_states <- categorical_count_renderer_process(
+    renderer,
+    state,
+    c('S', 'I')
   )
 
-  execute_process(render_states, sim$cpp_api)
+  execute_process(render_states, 1)
 
-  sim$r_api$queue_state_update(human, I, c(3, 6))
-  state_apply_updates(sim$state)
-  scheduler_tick(sim$scheduler)
+  state$queue_update('I', c(3, 6))
+  state$.update()
 
-  execute_process(render_states, sim$cpp_api)
+  execute_process(render_states, 2)
 
   rendered <- render$to_dataframe()
   expected <- data.frame(
     timestep = c(1, 2),
-    human_S_count = c(10, 8),
-    human_I_count = c(100, 102),
-    human_R_count = c(0, 0)
+    S_count = c(10, 8),
+    I_count = c(100, 102),
+    R_count = c(0, 0)
   )
   expect_mapequal(rendered, expected)
 })
 
 test_that("Prefab variable summaries work correctly", {
   size <- 110
-  S <- State$new('S', 10)
-  I <- State$new('I', 100)
-  R <- State$new('R', 0)
-  sequence <- Variable$new('sequence', seq_len(size))
-  sequence_2 <- Variable$new('sequence_2', seq_len(size) * 10)
-  human <- Individual$new('human', list(S, I, R), list(sequence, sequence_2))
+  sequence <- DoubleVariable$new(seq_len(size))
   render <- Render$new(2)
-  sim <- setup_simulation(list(human), renderer = render)
 
   render_variables <- variable_mean_renderer_process(
-    human$name,
-    c(sequence$name, sequence_2$name)
+    'sequence',
+    sequence
   )
 
-  execute_process(render_variables, sim$cpp_api)
+  execute_process(render_variables, 1)
 
-  sim$r_api$queue_variable_update(human, sequence, c((1:50) * 2, 51:110))
-  state_apply_updates(sim$state)
-  scheduler_tick(sim$scheduler)
+  sequence$queue_update(c((1:50) * 2, 51:110))
+  state$.update()
 
-  execute_process(render_variables, sim$cpp_api)
+  execute_process(render_variables, 2)
 
   rendered <- render$to_dataframe()
   expected <- data.frame(
     timestep = c(1, 2),
-    human_sequence_mean = c(55.5, 67.09091),
-    human_sequence_2_mean = c(555, 555)
+    sequence = c(55.5, 67.09091)
   )
   expect_mapequal(rendered, expected)
 })
