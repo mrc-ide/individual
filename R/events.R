1c826c0650c1ca75d6448f4e348e5e613817a871
Giovanni Charles <giovanni.charles@gmail.com>
2021-01-05 16:05:53 +0000

    Sketch of API refactor

--- R/events.R
+++ R/events.R
@@ -1,12 +1,11 @@
 #' Class: Event
-#' Describes an event in the simulation
+#' Describes a general event in the simulation
 #' @export Event
 Event <- R6::R6Class(
   'Event',
   public = list(
 
-    #' @field name the unique name of the event
-    name = '',
+    .event = NULL,
 
     #' @field listeners the listener functions to be executed when the event is
     #' triggered
@@ -14,15 +13,74 @@ Event <- R6::R6Class(
 
     #' @description Initialise an Event
     #' @param name, the name of the event
-    initialize = function(name) {
-      self$name <- name
+    initialize = function() {
+      self$.event <- create_event()
     },
 
     #' @description Add an event listener
-    #' @param listener the function to be executed on the event. This function
-    #' should take the simulation api and target individuals as arguments
+    #' @param listener the function to be executed on the event
     add_listener = function(listener) {
       self$listeners <- c(self$listeners, listener)
+    },
+
+    #' @description schedule this event to occur in the future
+    #' @param delay the number of timesteps to wait before triggering the event,
+    #' can be a scalar or an vector of values for several times in the future
+    schedule = function(delay) event_schedule(self$.event, delay),
+
+    #' @description Get the individuals who are scheduled
+    get_scheduled = function() {
+      event_get_scheduled(self$.event)
+    },
+
+    #' @description Stop a future event from triggering
+    clear_schedule = function(event) {
+      event_clear_schedule(self$.event)
+    },
+
+    #' @noRd
+    .tick = function() event_tick(self$.event),
+
+    #' @noRd
+    .process = function() event_process(self$listeners)
+  )
+)
+
+#' Class: TargetedEvent
+#' Describes a targeted event in the simulation
+#' This is useful for events which are triggered for a sub-population
+#' @export Event
+TargetedEvent <- R6::R6Class(
+  c('TargetedEvent', 'Event'),
+  public = list(
+    #' @description Initialise a Triggered event
+    #' @param population_size the size of the target population
+    initialize = function(population_size) {
+      self$.event <- create_targeted_event(population_size)
+    },
+
+    #' @description schedule this event to occur in the future
+    #' @param target the individuals to pass to the listener
+    #' @param delay the number of timesteps to wait before triggering the event,
+    #' can be a scalar or a vector of values for each target individual
+    schedule = function(target, delay) {
+      if (length(delay) == 1) {
+        targeted_event_schedule(self$.event, target, delay)
+      } else {
+        if (length(target) != length(delay)) {
+          stop(paste0(
+            event$name,
+            ' scheduled with a target which is a different size to delay'
+          ))
+        }
+        targeted_event_schedule_multi_delay(self$.event, target, delay)
+      }
+    }
+
+    #' @description Stop a future event from triggering for a subset of individuals
+    #' @param target the individuals to clear
+    clear_schedule = function(target) {
+      targeted_event_clear_schedule(self$.event, target)
     }
   )
 )
--- R/simulation.R
+++ R/simulation.R
@@ -1,25 +1,20 @@
-#' @title Main simulation loop
+#' @title A premade simulation loop
 #'
-#' @param individuals a list of Individual to simulate
+#' @param variables a list of Variables
+#' @param events a list of Events
 #' @param processes a list of processes to execute on each timestep
-#' @param end_timestep the number of timesteps to simulate
+#' @param timesteps the number of timesteps to simulate
 #' @param parameters a list of named parameters to pass to the process functions
-#' @param initialisation an optional function to initialise the model. This
-#' function is passed the API, which allows you to schedule initial events
 #' @examples
 #' population <- 4
-#' S <- State$new('S', population)
-#' I <- State$new('I', 0)
-#' R <- State$new('R', 0)
-#' human <- Individual$new('human', list(S, I, R))
+#' state <- CategoricalVariable$new(c('S', 'I', 'R'), rep('S', population))
 #'
 #' transition <- function(from, to, rate) {
 #'   return(function(api) {
-#'     from_state <- api$get_state(human, from)
-#'     api$queue_state_update(
-#'       human,
+#'     from_state <- state$get_index_of(from)
+#'     state$queue_update(
 #'       to,
-#'       from_state[runif(length(from_state), 0, 1) < rate]
+#'       from_state$sample(rate)
 #'     )
 #'   })
 #' }
@@ -30,65 +25,41 @@
 #'   transition(R, S, .05)
 #' )
 #'
-#' simulate(human, processes, 5)
-#' @export simulate
-simulate <- function(
-  individuals,
+#' simulation_loop(processes=processes, timesteps=5)
+#' @export
+simulation_loop <- function(
+  variables,
+  events,
   processes,
-  end_timestep,
-  parameters = list(),
-  initialisation = NULL
+  timesteps
   ) {
-  if (end_timestep <= 0) {
+  if (timesteps <= 0) {
     stop('End timestep must be > 0')
   }
-  if (!is.list(individuals)) {
-    individuals <- list(individuals)
-  }
-  render <- Render$new(end_timestep)
-  scheduler <- create_scheduler(individuals)
-  state <- create_state(individuals)
-  cpp_api <- create_process_api(state, scheduler, parameters, render)
-  api <- SimAPI$new(cpp_api, parameters, render)
-  if (!is.null(initialisation)) {
-    execute_any_process(initialisation, api, cpp_api)
-  }
-  for (t in seq_len(end_timestep)) {
+  for (t in seq_len(timesteps)) {
     for (process in processes) {
-      execute_any_process(process, api, cpp_api)
+      execute_any_process(process, t)
     }
-    scheduler_process_events(scheduler, cpp_api, api)
-    state_apply_updates(state)
-    scheduler_tick(scheduler)
+    for (event in events) {
+      event$.process()
+    }
+    for (variable in variables) {
+      variable$.update()
+    }
+    for (event in events) {
+      event$.tick()
+    }
   }
-  render$to_dataframe()
 }
 
 #' @title Execute a cpp or R process in the simulation
 #' @param p the process to execute
-#' @param api the R api
-#' @param cpp_api the C++ api
-execute_any_process <- function(p, api, cpp_api) {
+#' @param t the timestep to pass to the process
+#' @noRd
+execute_any_process <- function(p, t) {
   if (inherits(p, "externalptr")) {
-    execute_process(p, cpp_api)
+    execute_process(p, t)
   } else {
-    p(api)
+    p(t)
   }
-}
-
-#' @title Create a simulation state
-#' @param individuals a list of individual objects
-create_state <- function(individuals) {
-  individual_names <- vcapply(individuals, function(i) i$name)
-  individual_sizes <- vnapply(individuals, function (i) i$population_size)
-  state <- create_cpp_state(individual_names, individual_sizes)
-  for (i in individuals) {
-    state_names <- vcapply(i$states, function(s) s$name)
-    state_sizes <- vnapply(i$states, function(s) s$initial_size)
-    state_add_states(state, i$name, state_names, state_sizes)
-    for (v in i$variables) {
-      state_add_variable(state, i$name, v$name, v$initial_values)
-    }
-  }
-  state
 }
