bca172accbca15ac3d548931d27d829564b8f170
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-02-16 18:04:53 -0800

    IntegerVariable interface done; write tests

--- R/integer_variable.R
+++ R/integer_variable.R
@@ -12,29 +12,88 @@ IntegerVariable <- R6::R6Class(
     .variable = NULL,
 
     #' @description Create a new IntegerVariable
-    #' @param categories a character vector of possible values
-    #' @param initial_values a character vector of the initial value for each
+    #' @param initial_values a vector of the initial value for each which will be coerced to integer type
     #' individual
     initialize = function(initial_values) {
-      self$.variable <- create_integer_variable(categories, initial_values)
+      self$.variable <- create_integer_variable(as.integer(initial_values))
     },
 
-    #' @description return a bitset for individuals with the given `values`
-    #' @param values the values to filter
-    get_index_of = function(values) {
-      Bitset$new(from = categorical_variable_get_index_of(self$.variable, values))
-    },
-
-    #' @description queue an update for this variable
-    #' @param values the values to filter
-    queue_update = function(value, index) {
+    #' @description get the variable values
+    #' @param index optionally return a subset of the variable vector
+    get_values = function(index=NULL) {
+      if (is.null(index)) {
+        return(integer_variable_get_values(self$.variable))
+      }
       if (is.numeric(index)) {
-        categorical_variable_queue_update_vector(self$.variable, value, index)
-      } else {
-        categorical_variable_queue_update(self$.variable, value, index$.bitset)
+        return(integer_variable_get_values_at_index_vector(self$.variable, index))
+      }
+      integer_variable_get_values_at_index(self$.variable, index$.bitset)
+    },
+
+
+    #' @description return a bitset for individuals with some subset of values
+    #' Either search for indices corresponding to values in \code{set}, or
+    #' for indices corresponding to values in range [a,b].
+    #' @param set a vector of values 
+    #' @param a lower bound
+    #' @param b upper bound
+    get_index_of = function(set = NULL, a = NULL, b = NULL) {        
+        if(!is.null(set)) {
+            Bitset$new(from = integer_variable_get_index_of_set(self$.variable, set))
+        }
+        if(!is.null(a) & !is.null(b)) {
+            stopifnot(a < b)
+            Bitset$new(from = integer_variable_get_index_of_range(self$.variable, a, b))
+        }
+        stop("please provide a set of values to check, or both bounds of range [a,b]")        
+    },
+
+    #' @description Queue an update for a variable. There are 4 types of variable update:
+    #'
+    #' 1. Subset update. The index vector represents a subset of the variable to
+    #' update. The value vector, of the same size, represents the new values for
+    #' that subset
+    #' 2. Subset fill. The index vector represents a subset of the variable to
+    #' update. The value vector, of size 1, will fill the specified subset
+    #' 3. Variable reset. The index vector is set to `NULL` and the value vector
+    #' replaces all of the current values in the simulation. The value vector is
+    #' should match the size of the population.
+    #' 4. Variable fill. The index vector is set to `NULL` and the value vector,
+    #' of size 1, is used to fill all of the variable values in the population.
+    #' @param values a vector or scalar of values to assign at the index
+    #' @param index is the index at which to apply the change, use NULL for the
+    #' fill options
+    queue_update = function(values, index = NULL) {
+      if(is.null(index)){
+        if(length(values) == 1){
+          integer_variable_queue_fill(
+            self$.variable,
+            values
+          )
+        } else {
+          integer_variable_queue_update(
+            self$.variable,
+            values,
+            numeric(0)
+          )
+        }
+      } else if(is.numeric(index)) {
+        if (length(index) != 0) {
+          integer_variable_queue_update(
+            self$.variable,
+            values,
+            index
+          )
+        }
+      } else if(!is.null(index$.bitset)) {
+        integer_variable_queue_update(
+          self$.variable,
+          values,
+          index$to_vector()
+        )
       }
     },
 
-    .update = function() categorical_variable_update(self$.variable)
+    .update = function() integer_variable_update(self$.variable)
   )
 )
--- src/integer_variable.cpp
+++ src/integer_variable.cpp
@@ -1,58 +1,92 @@
 /*
- * state.cpp -> variable.cpp
+ * IntegerVariable.cpp
  *
- *  Created on: 19 May 2020
- *      Author: gc1610
+ *  Created on: 16 Feb 2021
+ *      Author: slwu89
  */
 
 
-#include "../inst/include/CategoricalVariable.h"
+#include "../inst/include/IntegerVariable.h"
 #include "utils.h"
 
 //[[Rcpp::export]]
-Rcpp::XPtr<CategoricalVariable> create_categorical_variable(
-    const std::vector<std::string>& categories,
-    const std::vector<std::string>& values 
+Rcpp::XPtr<IntegerVariable> create_integer_variable(
+    const std::vector<int>& values
     ) {
-    return Rcpp::XPtr<CategoricalVariable>(
-        new CategoricalVariable(categories, values),
+    return Rcpp::XPtr<IntegerVariable>(
+        new IntegerVariable(values),
         true
     );
 }
 
 //[[Rcpp::export]]
-void categorical_variable_queue_update(
-    Rcpp::XPtr<CategoricalVariable> variable,
-    const std::string& value,
+std::vector<int> integer_variable_get_values(
+    Rcpp::XPtr<IntegerVariable> variable
+    ) {
+    return variable->get_values();
+}
+
+//[[Rcpp::export]]
+std::vector<int> integer_variable_get_values_at_index(
+    Rcpp::XPtr<IntegerVariable> variable,
     Rcpp::XPtr<individual_index_t> index
     ) {
-    variable->queue_update(value, *index);
+    return variable->get_values(*index);
 }
 
 //[[Rcpp::export]]
-Rcpp::XPtr<individual_index_t> categorical_variable_get_index_of(
-    Rcpp::XPtr<CategoricalVariable> variable,
-    const std::vector<std::string>& values 
-    ) {
-    return Rcpp::XPtr<individual_index_t>(
-        new individual_index_t(variable->get_index_of(values)),
-        true
-    );
-}
-
-//[[Rcpp::export]]
-void categorical_variable_queue_update_vector(
-    Rcpp::XPtr<CategoricalVariable> variable,
-    const std::string& value,
-    std::vector<size_t>& index
+std::vector<int> integer_variable_get_values_at_index_vector(
+    Rcpp::XPtr<IntegerVariable> variable,
+    std::vector<size_t> index
     ) {
     decrement(index);
     auto bitmap = individual_index_t(variable->size);
-    bitmap.insert(index.begin(), index.end());
-    variable->queue_update(value, bitmap);
+    bitmap.insert_safe(index.begin(), index.end());
+    return variable->get_values(bitmap);
+}
+
+// [[Rcpp::export]]
+Rcpp::XPtr<individual_index_t> integer_variable_get_index_of_set(
+    Rcpp::XPtr<IntegerVariable> variable,
+    std::vector<int> values_set
+) {
+    return Rcpp::XPtr<individual_index_t>(
+        new individual_index_t(variable->get_index_of_set(values_set)),
+        true
+    );
+}
+
+// [[Rcpp::export]]
+Rcpp::XPtr<individual_index_t> integer_variable_get_index_of_range(
+    Rcpp::XPtr<IntegerVariable> variable,
+    const int a,
+    const int b
+) {
+    return Rcpp::XPtr<individual_index_t>(
+        new individual_index_t(variable->get_index_of_range(a, b)),
+        true
+    );
 }
 
 //[[Rcpp::export]]
-void categorical_variable_update(Rcpp::XPtr<CategoricalVariable> variable) {
+void integer_variable_queue_fill(
+    Rcpp::XPtr<IntegerVariable> variable,
+    const std::vector<int> value
+) {
+    variable->queue_update(value, std::vector<size_t>());
+}
+
+//[[Rcpp::export]]
+void integer_variable_queue_update(
+    Rcpp::XPtr<IntegerVariable> variable,
+    const std::vector<int> value,
+    std::vector<size_t> index
+) {
+    decrement(index);
+    variable->queue_update(value, index);
+}
+
+//[[Rcpp::export]]
+void integer_variable_update(Rcpp::XPtr<IntegerVariable> variable) {
     variable->update();
 }
