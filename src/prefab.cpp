190b12890a605c5f82c80f15f3546125e5c9ac49
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-03-16 18:20:34 -0700

    revert unwanted change to prefabs in this branch

--- src/prefab.cpp
+++ src/prefab.cpp
@@ -25,15 +25,18 @@ Rcpp::XPtr<process_t> fixed_probability_multinomial_process_internal(
 
     // make pointer to lambda function and return XPtr to R
     return Rcpp::XPtr<process_t>(
-        new process_t([variable,source_state,destination_states,rate,cdf](size_t t){      
+        new process_t([variable,source_state,destination_states,rate,cdf](size_t t){
 
             // sample leavers
             individual_index_t leaving_individuals(variable->get_index_of(source_state));
             bitset_sample_internal(leaving_individuals, rate);
 
             // empty bitsets to put them (their destinations)
+            std::vector<individual_index_t> destination_individuals;
             size_t n = destination_states.size();
-            std::vector<individual_index_t> destination_individuals(n, leaving_individuals.max_size());
+            for (size_t i=0; i<n; i++) {
+                destination_individuals.emplace_back(leaving_individuals.max_size());
+            }
 
             // random variate for each leaver to see where they go
             const auto random = Rcpp::runif(leaving_individuals.size());
@@ -70,7 +73,7 @@ Rcpp::XPtr<process_t> multi_probability_multinomial_process_internal(
 
     // make pointer to lambda function and return XPtr to R
     return Rcpp::XPtr<process_t>(
-        new process_t([variable,source_state,destination_states,rate_variable,cdf](size_t t){      
+        new process_t([variable,source_state,destination_states,rate_variable,cdf](size_t t){
 
             // sample leavers with their unique prob
             individual_index_t leaving_individuals(variable->get_index_of(source_state));
@@ -78,8 +81,11 @@ Rcpp::XPtr<process_t> multi_probability_multinomial_process_internal(
             bitset_sample_multi_internal(leaving_individuals, rate_vector.begin(), rate_vector.end());
 
             // empty bitsets to put them (their destinations)
+            std::vector<individual_index_t> destination_individuals;
             size_t n = destination_states.size();
-            std::vector<individual_index_t> destination_individuals(n, leaving_individuals.max_size());
+            for (size_t i=0; i<n; i++) {
+                destination_individuals.emplace_back(leaving_individuals.max_size());
+            }
 
             // random variate for each leaver to see where they go
             const auto random = Rcpp::runif(leaving_individuals.size());
@@ -111,7 +117,7 @@ Rcpp::XPtr<process_t> multi_probability_bernoulli_process_internal(
 
     // make pointer to lambda function and return XPtr to R
     return Rcpp::XPtr<process_t>(
-        new process_t([variable,rate_variable,from,to](size_t t){      
+        new process_t([variable,rate_variable,from,to](size_t t){
 
             // sample leavers with their unique prob
             individual_index_t leaving_individuals(variable->get_index_of(from));
