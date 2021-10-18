9d1805b7e5519c639d7d679ad87bb8784746d237
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-10-18 10:25:39 -0700

    clean bitset benchmark script

--- tests/performance/bench-bitset.R
+++ tests/performance/bench-bitset.R
@@ -2,6 +2,8 @@
 # currently we only benchmark erasure and insertion of elements,
 # eventually more (all?) of its functionality will be benchmarked
 
+rm(list = ls()); gc()
+
 library(individual)
 library(bench)
 library(ggplot2)
@@ -15,6 +17,19 @@ create_random_data <- function(size, limit) {
   stopifnot(is.finite(limit))
   stopifnot(limit > 0)
   sample.int(n = limit, size = size, replace = FALSE)
+}
+
+#' @title Create random bitset
+#' @param size the number of set bits
+#' @param limit the maximum size of the bitset
+create_random_bitset <- function(size, limit) {
+  stopifnot(is.finite(size))
+  stopifnot(is.finite(limit))
+  stopifnot(limit > 0)
+  bset <- individual::Bitset$new(size = limit)
+  bset$not(inplace = TRUE)
+  bset$choose(k = size)
+  return(bset)
 }
 
 #' @title Create grid of parameters for benchmarking
@@ -34,11 +49,25 @@ build_grid <- function(base1, base2, powers1) {
   do.call(what = rbind, args = grid)
 }
 
-# core operations
-
 # limit: max size of bitset
 # size: number of elements to be inserted
 args_grid <- build_grid(base1 = 10, base2 = 5, powers1 = 5)
+
+
+#' @title Simplify output of [bench::press] for plotting
+#' @description Unnest output to generate histograms or density plots, and remove
+#' all runs where any level of garbage collection was executed.
+#' @param out output of [bench::press] function
+simplify_bench_output <- function(out) {
+  out <- tidyr::unnest(out, c(time, gc))
+  out <- out[out$gc == "none", ]
+  return(out)
+}
+
+
+# ------------------------------------------------------------
+# benchmark: core operations
+# ------------------------------------------------------------
 
 insert_bset <- bench::press(
  {
@@ -85,33 +114,111 @@ erase_bset <- bench::press(
   .grid = args_grid
 )
 
-erase_bset <- tidyr::unnest(erase_bset, c(time, gc))
-erase_bset <- erase_bset[erase_bset$gc == "none", ]
+erase_bset <- simplify_bench_output(out = erase_bset)
 
 ggplot(data = erase_bset) +
   geom_violin(aes(x = as.factor(size), y = time))
 
-iterate_bset <- tidyr::unnest(iterate_bset, c(time, gc))
-iterate_bset <- iterate_bset[iterate_bset$gc == "none", ]
+iterate_bset <- simplify_bench_output(out = iterate_bset)
 
 ggplot(data = iterate_bset) +
   geom_violin(aes(x = as.factor(size), y = time))
 
-insert_bset <- tidyr::unnest(insert_bset, c(time, gc))
-insert_bset <- insert_bset[insert_bset$gc == "none", ]
+insert_bset <- simplify_bench_output(out = insert_bset)
 
 ggplot(data = insert_bset) +
   geom_violin(aes(x = as.factor(size), y = time))
 
-# set operations
-# or_bset
-# and_bset
-# not_bset
-# xor_bset
-# set_diff_bset
 
-# sampling operations
-# sample_bset
+# ------------------------------------------------------------
+# benchmark: set operations
+# ------------------------------------------------------------
+
+or_bset <- bench::press(
+  {
+    index1 <- create_random_bitset(size = size, limit = limit)
+    index2 <- create_random_bitset(size = size, limit = limit)
+    bench::mark(
+      min_iterations = 50,
+      check = FALSE, 
+      filter_gc = TRUE,
+      {index1$or(index2)},
+    )
+  }, 
+  .grid = args_grid
+) 
+
+and_bset <- bench::press(
+  {
+    index1 <- create_random_bitset(size = size, limit = limit)
+    index2 <- create_random_bitset(size = size, limit = limit)
+    bench::mark(
+      min_iterations = 50,
+      check = FALSE, 
+      filter_gc = TRUE,
+      {index1$and(index2)},
+    )
+  }, 
+  .grid = args_grid
+) 
+
+not_bset <- bench::press(
+  {
+    index <- create_random_bitset(size = size, limit = limit)
+    bench::mark(
+      min_iterations = 50,
+      check = FALSE, 
+      filter_gc = TRUE,
+      {index$not(inplace = TRUE)},
+    )
+  }, 
+  .grid = args_grid
+) 
+
+xor_bset <- bench::press(
+  {
+    index1 <- create_random_bitset(size = size, limit = limit)
+    index2 <- create_random_bitset(size = size, limit = limit)
+    bench::mark(
+      min_iterations = 50,
+      check = FALSE, 
+      filter_gc = TRUE,
+      {index1$xor(index2)},
+    )
+  }, 
+  .grid = args_grid
+) 
+
+set_diff_bset <- bench::press(
+  {
+    index1 <- create_random_bitset(size = size, limit = limit)
+    index2 <- create_random_bitset(size = size, limit = limit)
+    bench::mark(
+      min_iterations = 50,
+      check = FALSE, 
+      filter_gc = TRUE,
+      {index1$set_difference(index2)},
+    )
+  }, 
+  .grid = args_grid
+)
+
+or_bset <- simplify_bench_output(out = or_bset)
+and_bset <- simplify_bench_output(out = and_bset)
+not_bset <- simplify_bench_output(out = not_bset)
+xor_bset <- simplify_bench_output(out = xor_bset)
+set_diff_bset <- simplify_bench_output(out = set_diff_bset)
+
+set_ops_bset <- rbind(or_bset, and_bset, not_bset, xor_bset, set_diff_bset)
+
+ggplot(data = set_ops_bset) +
+  geom_violin(aes(x = as.factor(size), y = time, color = as.factor(size), fill = as.factor(size))) +
+  facet_wrap(. ~ expression, scales = "free")
+
+
+# ------------------------------------------------------------
+# benchmark: sampling operations
+# ------------------------------------------------------------
 
 choose_bset <- bench::press(
   {
@@ -126,8 +233,7 @@ choose_bset <- bench::press(
   .grid = args_grid
 ) 
 
-choose_bset <- tidyr::unnest(choose_bset, c(time, gc))
-choose_bset <- choose_bset[choose_bset$gc == "none", ]
+choose_bset <- simplify_bench_output(choose_bset)
 
 ggplot(data = choose_bset) +
   geom_violin(aes(x = as.factor(size), y = time))
@@ -148,8 +254,7 @@ filter_bset <- bench::press(
   .grid = args_grid
 ) 
 
-filter_bset <- tidyr::unnest(filter_bset, c(time, gc))
-filter_bset <- filter_bset[filter_bset$gc == "none", ]
+filter_bset <- simplify_bench_output(filter_bset)
 
 ggplot(data = filter_bset) +
   geom_violin(aes(x = expression, y = time, color = expression, fill = expression)) +
---
+++ tests/performance/bench-doublevariable.R
@@ -0,0 +1,172 @@
+# need to check queueing updates and applying updates for:
+# single value, bitset index
+# many values bitset index
+# single value, vector index
+# many values, vector index
+
+library(individual)
+library(bench)
+library(ggplot2)
+library(tidyr)
+
+# for each we want to run multiple times and apply the update.
+# each time we can use random indices.
+
+#' @title Create random integer vector
+#' @param size the size of the vector to be produced
+#' @param limit random elements will be between 1 and limit
+create_random_index_vector <- function(size, limit) {
+  stopifnot(is.finite(size))
+  stopifnot(is.finite(limit))
+  stopifnot(limit > 0)
+  sample.int(n = limit, size = size, replace = FALSE)
+}
+
+#' @title Create random bitset
+#' @param size the number of set elements in the bitset
+#' @param limit maximum size of the bitset
+create_random_index_bitset <- function(size, limit) {
+  stopifnot(is.finite(size))
+  stopifnot(is.finite(limit))
+  stopifnot(limit > 0)
+  bset <- individual::Bitset$new(size = limit)
+  bset$not(inplace = TRUE)
+  bset$choose(k = size)
+  return(bset)
+}
+
+#' @title Create grid of parameters for benchmarking
+#' @description First, construct a grid of values raising `base1` to powers in
+#' `powers1` (the major sequence).
+#' @param base1 the base of the first (major) sequence
+#' @param base2 the base of the second (minor) sequence, should be less than `base1`
+#' @param powers1 the sequence of powers for the first sequence, should not include powers < 1
+#' @param n the number of times to run each combination, or extra integer argument.
+build_grid_2base <- function(base1, base2, powers1, n) {
+  stopifnot(base2 < base1)
+  stopifnot(min(powers1) > 1)
+  
+  grid <- lapply(X = powers1, FUN = function(x) {
+    lim <- base1^x
+    y <- floor(log(base1^x) / log(base2))
+    size_seq <- base2^(1:y)
+    data.frame("limit" = lim, "size" = size_seq, "n" = as.integer(n))
+  })
+  do.call(what = rbind, args = grid)
+}
+
+# base1 is for maximal population size
+# base2 is for the updating size
+# n is the number of updates that are queued
+args_grid <- build_grid_2base(base1 = 10, base2 = 5, powers1 = c(3, 5), n = 3)
+
+
+# single value, bitset index
+update_sv_bi <- bench::press(
+  {
+    variable <- individual::DoubleVariable$new(initial_values = rep(1, limit))
+    indices <- lapply(X = 1:n, FUN = function(nn){
+      create_random_index_bitset(size = size, limit = limit)
+    })
+    value <- 0
+    bench::mark(
+      min_iterations = 50,
+      check = FALSE, 
+      filter_gc = TRUE,
+      {
+        lapply(X = indices, FUN = function(b){
+          variable$queue_update(values = value, index = b)
+        })
+        variable$.update()
+      }
+    )
+  }, 
+  .grid = args_grid
+)
+
+# vector value, bitset index
+update_vv_bi <- bench::press(
+  {
+    variable <- individual::DoubleVariable$new(initial_values = rep(1, limit))
+    indices <- lapply(X = 1:n, FUN = function(nn){
+      create_random_index_bitset(size = size, limit = limit)
+    })
+    value <- rep(0, size)
+    bench::mark(
+      min_iterations = 50,
+      check = FALSE,
+      filter_gc = TRUE,
+      {
+        lapply(X = indices, FUN = function(b){
+          variable$queue_update(values = value, index = b)
+        })
+        variable$.update()
+      }
+    )
+  }, 
+  .grid = args_grid
+)
+
+# single value, vector index
+update_sv_vi <- bench::press(
+  {
+    variable <- individual::DoubleVariable$new(initial_values = rep(1, limit))
+    indices <- lapply(X = 1:n, FUN = function(nn){
+      create_random_index_bitset(size = size, limit = limit)$to_vector()
+    })
+    value <- 0
+    bench::mark(
+      min_iterations = 50,
+      check = FALSE,
+      filter_gc = TRUE,
+      {
+        lapply(X = indices, FUN = function(b){
+          variable$queue_update(values = value, index = b)
+        })
+        variable$.update()
+      }
+    )
+  }, 
+  .grid = args_grid
+)
+
+# vector value, vector index
+update_vv_vi <- bench::press(
+  {
+    variable <- individual::DoubleVariable$new(initial_values = rep(1, limit))
+    indices <- lapply(X = 1:n, FUN = function(nn){
+      create_random_index_bitset(size = size, limit = limit)$to_vector()
+    })
+    value <- rep(0, size)
+    bench::mark(
+      min_iterations = 50,
+      check = FALSE,
+      filter_gc = TRUE,
+      {
+        lapply(X = indices, FUN = function(b){
+          variable$queue_update(values = value, index = b)
+        })
+        variable$.update()
+      }
+    )
+  },
+  .grid = args_grid
+)
+
+update_sv_bi$type <- "sv-bi"
+update_vv_bi$type <- "vv-bi"
+update_sv_vi$type <- "sv-vi"
+update_vv_vi$type <- "vv-vi"
+
+update_sv_bi <- tidyr::unnest(update_sv_bi, c(time, gc))
+update_vv_bi <- tidyr::unnest(update_vv_bi, c(time, gc))
+update_sv_vi <- tidyr::unnest(update_sv_vi, c(time, gc))
+update_vv_vi <- tidyr::unnest(update_vv_vi, c(time, gc))
+
+update_all <- rbind(update_sv_bi, update_vv_bi, update_sv_vi, update_vv_vi)
+update_all <- update_all[update_all$gc == "none", ]
+
+ggplot(data = update_all) +
+  geom_violin(aes(type, time, fill = type, color = type)) +
+  facet_wrap(limit ~ size, scales = "free", labeller = label_context)
+              
