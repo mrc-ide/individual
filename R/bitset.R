f465f8e56c8d48e183ba3a482a98b9c3ea48b9ff
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-10-22 17:27:56 -0700

    remove all from stopifnot

--- R/bitset.R
+++ R/bitset.R
@@ -101,7 +101,7 @@ Bitset <- R6Class(
       if (length(rate) == 1) {
         bitset_sample(self$.bitset, rate)
       } else {
-        stopifnot(all(is.finite(rate)))
+        stopifnot(is.finite(rate))
         bitset_sample_vector(self$.bitset, rate)
       }
       self
