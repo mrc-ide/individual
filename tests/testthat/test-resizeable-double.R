e6ba7c79da5e70907bc27a55e4340c9ee2c203f6
Giovanni Charles <giovanni.charles@gmail.com>
2021-11-12 11:30:57 +0000

    Reorganise variable tests

--- tests/testthat/test-resizeable-double.R
+++ tests/testthat/test-resizeable-double.R
@@ -135,3 +135,107 @@ test_that("get index of method fails correctly with bad inputs", {
   expect_error(x$get_index_of(a = 5))
   expect_error(x$get_index_of(b = 5))
 })
+
+test_that("updating variables works", {
+  size <- 10
+  sequence <- ResizeableDoubleVariable$new(seq_len(size))
+
+  first <- sequence$get_values()
+  sequence$queue_update((1:5) * 2, 1:5)
+  sequence$.update()
+  middle <- sequence$get_values()
+  sequence$queue_update(11, 2:6)
+  sequence$.update()
+  last <- sequence$get_values()
+
+  expect_equal(first, 1:10)
+  expect_equal(middle, c((1:5) * 2, 6:10))
+  expect_equal(last, c(2, rep(11, 5), 7:10))
+})
+
+test_that("updating variables at the boundaries works", {
+  size <- 10
+  sequence <- ResizeableDoubleVariable$new(seq_len(size))
+
+  before <- sequence$get_values()
+  sequence$queue_update(2, 10)
+  sequence$queue_update(2, 1)
+  sequence$.update()
+  after <- sequence$get_values()
+
+  expect_equal(before, 1:10)
+  expect_equal(after, c(2, 2:9, 2))
+})
+
+test_that("updating variables with an empty index is ignored", {
+  size <- 10
+  sequence <- ResizeableDoubleVariable$new(seq_len(size))
+
+  before <- sequence$get_values()
+  sequence$queue_update(11, numeric(0))
+  sequence$.update()
+  after <- sequence$get_values()
+
+  expect_equal(before, 1:10)
+  expect_equal(after, 1:10)
+})
+
+test_that("updating variables with an empty bitset is ignored", {
+  size <- 10
+  sequence <- ResizeableDoubleVariable$new(seq_len(size))
+
+  before <- sequence$get_values()
+  sequence$queue_update(11, Bitset$new(10))
+  sequence$.update()
+  after <- sequence$get_values()
+
+  expect_equal(before, 1:10)
+  expect_equal(after, 1:10)
+})
+
+test_that("updating variables with silly indices errors gracefully", {
+  size <- 10
+  sequence <- ResizeableDoubleVariable$new(seq_len(size))
+
+  # different sized values and index
+  expect_error(sequence$queue_update(c(1.0, 2.0), 1:5))
+
+  expect_error(sequence$queue_update(11, -1:3)) # invalid index
+
+  expect_error(sequence$queue_update(11, 9:15)) # out of bounds
+})
+
+test_that("Queuing non numeric values errors gracefully", {
+  size <- 10
+  sequence <- ResizeableDoubleVariable$new(seq_len(size))
+  expect_error(sequence$queue_update(c(1, "A"), 1:2))
+})
+
+test_that("updating the complete variable vector works", {
+  size <- 10
+  sequence <- ResizeableDoubleVariable$new(seq_len(size))
+
+  before <- sequence$get_values()
+
+  sequence$queue_update(11:20)
+  sequence$.update()
+
+  after <- sequence$get_values()
+
+  expect_equal(before, 1:10)
+  expect_equal(after, 11:20)
+})
+
+test_that("Vector fill variable updates work", {
+  size <- 10
+  sequence <- ResizeableDoubleVariable$new(seq_len(size))
+
+  before <- sequence$get_values()
+  sequence$queue_update(14)
+  sequence$.update()
+  after <- sequence$get_values()
+
+  expect_equal(before, 1:10)
+  expect_equal(after, rep(14, 10))
+})
+
