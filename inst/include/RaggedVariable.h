b13cc67f703f2a69de004f03622488d1173f054e
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-07-07 13:42:36 -0700

    RaggedVariable header

---
+++ inst/include/RaggedVariable.h
@@ -0,0 +1,33 @@
+/*
+ * RaggedVariable.h
+ *
+ *  Created on: 7 Jul 2022
+ *      Author: slwu89
+ */
+
+#ifndef INST_INCLUDE_RAGGED_VARIABLE_H_
+#define INST_INCLUDE_RAGGED_VARIABLE_H_
+
+#include "Variable.h"
+#include "common_types.h"
+#include <Rcpp.h>
+
+
+// forward declaration
+template <class A>
+class RaggedVariable;
+
+
+
+// template <class A>
+// class RaggedVariable {
+//   
+//   std::vector<A> values;
+//   
+//   RaggedVariable(const std::vector<std::vector<T>>& initial_values);
+//   ~RaggedVariable() = default;
+// };
+
+
+
+#endif
