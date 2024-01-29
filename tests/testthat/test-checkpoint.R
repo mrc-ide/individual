4fb91273e21f70076a0347306865b3170f3a8260
Paul Liétar <pl2113@ic.ac.uk>
2024-01-29 16:49:08 +0000

    Don't restore the random number generator state by default. (#181)
    It can have suprising side effects by resetting the user global RNG
    state to a fixed value, and also get in the way of doing stochastic
    simulations where we actually want to sample many different futures from
    a given start point.
    
    The only time where a user actually wants to restore the random number
    generator state is probably when writing tests that compare a continuous
    run against a resumed one. They can opt-in to that behaviour with a new
    argument to `simulation_loop` called `restore_random_state`.

--- tests/testthat/test-checkpoint.R
+++ tests/testthat/test-checkpoint.R
@@ -1,5 +1,5 @@
 test_that("deterministic state model is resumable", {
-  simulation <- function(timesteps, state=NULL) {
+  simulation <- function(timesteps, ...) {
     population <- 10
     health <- CategoricalVariable$new(c('S', 'I', 'R'), rep('S', population))
     render <- Render$new(timesteps)
@@ -24,13 +24,13 @@ test_that("deterministic state model is resumable", {
       variables = list(health),
       processes = processes,
       timesteps = timesteps,
-      state = state
+      ...
     )
-    list(state=new_state, data=render$to_dataframe())
+    list(state = new_state, data = render$to_dataframe())
   }
 
-  first_phase <- simulation(5, state=NULL)
-  second_phase <- simulation(10, state=first_phase$state)
+  first_phase <- simulation(5)
+  second_phase <- simulation(10, state = first_phase$state)
 
   expected <- data.frame(
     timestep = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
@@ -38,7 +38,12 @@ test_that("deterministic state model is resumable", {
     I_count = c(0, 2, 3, 4, 5, 6, 5, 4, 3, 2),
     R_count = c(0, 0, 1, 2, 3, 4, 5, 6, 7, 8)
   )
-  expected_na <- data.frame(timestep=1:5, S_count=NA_real_, I_count=NA_real_, R_count=NA_real_)
+  expected_na <- data.frame(
+    timestep = 1:5,
+    S_count=NA_real_,
+    I_count=NA_real_,
+    R_count=NA_real_
+  )
 
   expect_mapequal(first_phase$data, expected[1:5,])
   expect_mapequal(second_phase$data[1:5,], expected_na)
@@ -46,7 +51,7 @@ test_that("deterministic state model is resumable", {
 })
 
 test_that("deterministic model with events is resumable", {
-  simulation <- function(timesteps, state=NULL) {
+  simulation <- function(timesteps, ...) {
     population <- 10
     health <- CategoricalVariable$new(c('S', 'I', 'R'), rep('S', population))
     render <- Render$new(timesteps)
@@ -83,13 +88,13 @@ test_that("deterministic model with events is resumable", {
       events = list(infection, recovery),
       processes = processes,
       timesteps = timesteps,
-      state = state
+      ...
     )
-    list(state=new_state, data=render$to_dataframe())
+    list(state = new_state, data = render$to_dataframe())
   }
 
-  first_phase <- simulation(5, state=NULL)
-  second_phase <- simulation(15, state=first_phase$state)
+  first_phase <- simulation(5)
+  second_phase <- simulation(15, state = first_phase$state)
 
   expected <- data.frame(
     timestep = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
@@ -97,33 +102,55 @@ test_that("deterministic model with events is resumable", {
     I_count = c(0, 0, 2, 4, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1, 0),
     R_count = c(0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
   )
-  expected_na <- data.frame(timestep=1:5, S_count=NA_real_, I_count=NA_real_, R_count=NA_real_)
+  expected_na <- data.frame(
+    timestep = 1:5,
+    S_count=NA_real_,
+    I_count=NA_real_,
+    R_count=NA_real_
+  )
 
   expect_mapequal(first_phase$data, expected[1:5,])
   expect_mapequal(second_phase$data[1:5,], expected_na)
   expect_mapequal(second_phase$data[6:15,], expected[6:15,])
 })
 
-
-test_that("stochastic simulation is repeatable", {
-  simulation <- function(timesteps, state = NULL) {
-    render <- Render$new(timesteps)
-    p <- function(t) {
-      render$render("value", runif(1), t)
-    }
-    new_state <- simulation_loop(
-      processes = list(p),
-      timesteps = timesteps,
-      state = state
-    )
-    list(state=new_state, data=render$to_dataframe())
+stochastic_model <- function(timesteps, ...) {
+  render <- Render$new(timesteps)
+  p <- function(t) {
+    render$render("value", runif(1), t)
   }
+  new_state <- simulation_loop(
+    processes = list(p),
+    timesteps = timesteps,
+    ...
+  )
+  list(state = new_state, data = render$to_dataframe())
+}
 
-  initial <- simulation(5)$state
-  first <- simulation(10, state=initial)$data
-  second <- simulation(10, state=initial)$data
+test_that("stochastic simulations are resumed independently by default", {
+  initial_state <- stochastic_model(5)$state
+  first_run <- stochastic_model(10, state = initial_state)$data
+  second_run <- stochastic_model(10, state = initial_state)$data
 
-  expect_equal(first, second)
+  expect_false(isTRUE(all.equal(first_run, second_run)))
+})
+
+test_that("stochastic simulation can be resumed deterministically", {
+  set.seed(123)
+  initial <- stochastic_model(5)
+  first_run <- stochastic_model(
+    10, state = initial$state, restore_random_state = TRUE)$data
+  second_run <- stochastic_model(
+    10, state = initial$state, restore_random_state = TRUE)$data
+
+  expect_mapequal(first_run, second_run)
+
+  set.seed(123)
+  contiguous_run <- stochastic_model(10)$data
+
+  expect_mapequal(contiguous_run[1:5,], initial$data)
+  expect_mapequal(contiguous_run[6:10,], first_run[6:10,])
+  expect_mapequal(contiguous_run[6:10,], second_run[6:10,])
 })
 
 test_that("cannot add nor remove variables when resuming", {
@@ -131,13 +158,13 @@ test_that("cannot add nor remove variables when resuming", {
     lapply(seq_len(count), function(i) DoubleVariable$new(1:10))
   }
 
-  state <- simulation_loop(timesteps=5, variables=make_variables(2))
+  state <- simulation_loop(timesteps = 5, variables = make_variables(2))
 
   expect_error(
-    simulation_loop(timesteps=10, variables=make_variables(1), state=state),
+    simulation_loop(timesteps = 10, variables = make_variables(1), state = state),
     "Checkpoint's variables do not match simulation's")
   expect_error(
-    simulation_loop(timesteps=10, variables=make_variables(3), state=state),
+    simulation_loop(timesteps = 10, variables = make_variables(3), state = state),
     "Checkpoint's variables do not match simulation's")
 })
 
@@ -145,10 +172,10 @@ test_that("cannot resume with smaller timesteps", {
   state <- simulation_loop(timesteps = 10)
 
   expect_error(
-    simulation_loop(timesteps=5, state=state),
+    simulation_loop(timesteps = 5, state = state),
     "Restored state is already longer than timesteps")
 
   expect_error(
-    simulation_loop(timesteps=10, state=state),
+    simulation_loop(timesteps = 10, state = state),
     "Restored state is already longer than timesteps")
 })
